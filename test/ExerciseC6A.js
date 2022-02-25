a69c360bd9a1c8151654664643ced7241fbda5fb
scheuclu <scheuclu@gmail.com>
2022-02-25 17:03:29 +0100

    Finished ExerciseC6A

--- test/ExerciseC6A.js
+++ test/ExerciseC6A.js
@@ -1,5 +1,6 @@
 
 var Test = require('../config/testConfig.js');
+const truffleAssert = require('truffle-assertions');
 
 contract('ExerciseC6A', async (accounts) => {
 
@@ -7,6 +8,34 @@ contract('ExerciseC6A', async (accounts) => {
   before('setup contract', async () => {
     config = await Test.Config(accounts);
   });
+
+
+  it('contract operational status works properlu', async () => {
+    
+    // ARRANGE
+    let caller = accounts[0];
+
+    // ACT
+    //let result = await config.exerciseC6A.isOperational(caller);
+    let result = await config.exerciseC6A.isOperational({from: config.owner}); 
+
+    // ASSERT
+    assert.equal(result, false, "Contract should be inoperational initally");
+
+    // Set the status to operational
+    await config.exerciseC6A.setOperatingStatus(true, {from: config.owner});
+    result = await config.exerciseC6A.isOperational({from: config.owner});
+    assert.equal(result, true, "Contract should be operational now");
+
+
+    //Set the status from a non-owner account. this should fail
+    await truffleAssert.fails(
+      config.exerciseC6A.setOperatingStatus(true, {from: config.testAddresses[1]}),
+      "Caller is not contract owner"
+    );
+
+  });
+
 
   it('contract owner can register new user', async () => {
     
@@ -23,5 +52,4 @@ contract('ExerciseC6A', async (accounts) => {
 
   });
 
- 
 });
